546f82a921d2755e4cab71c4e7f9174e7b8561fe
Jonathan Lurie <lurie.jo@gmail.com>
2016-03-01 11:57:27 -0500

    Moving some stuff to make more generic pages

--- locate/js/core.js
+++ locate/js/core.js
@@ -1,3 +1,5 @@
+var headerImage = null;
+
 // smooth scrolling
 $(function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
@@ -14,21 +16,53 @@ $(function() {
     });
 });
 
-
+/*
 $(document).ready(function(){
+    console.log("ready!");
+    //loadRandomHeader();
+});
+
+$(document).load(function(){
+    console.log("loaded!");
+    //displayRandomHeader();
+});
+*/
+
+$(document).ready(function() {
+    // executes when HTML-Document is loaded and DOM is ready
+    console.log("document is ready");
+    loadRandomHeader();
+
+    console.log(window.location.hash);
+
+    if(window.location.hash && window.location.hash == "#thankyou") {
+        $("#thankYouMessage").removeClass("hidden");
+        window.location.href = "#contact";
+    }
+});
+
+
+$(window).load(function() {
+    // executes when complete page is fully loaded, including all frames, objects and images
+    console.log("window is loaded");
     displayRandomHeader();
 });
 
-$(document).on('ready', function(){
 
-})
-
-
-function displayRandomHeader(){
+function loadRandomHeader(){
     var min = 1;
     var max = 13;
     var random = Math.floor(Math.random() * (max - min + 1)) + min;
-    $(".intro-header").css("background", "linear-gradient( rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25) ), url(img/photos/80pc/splashscreen-" + random + ".jpg)");
+
+    headerImage = new Image();
+    headerImage.src = "../dependencies/img/photos/80pc/splashscreen-" + random + ".jpg";
+
+}
+
+function displayRandomHeader(){
+
+
+    $(".intro-header").css("background", "linear-gradient( rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25) ), url(" + headerImage.src + ")");
     $(".intro-header").css("background-size", "cover");
     $(".intro-header").css("background-repeat", "no-repeat");
     $(".intro-header").css("background-position", "center");
